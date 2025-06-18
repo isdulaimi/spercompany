16c937d1569664c09db689d3b8bbca77bd6a673e
isdulaimi <isdulaimi@gmail.com>
2025-06-19 00:59:39 +0300

    Add files via upload

---
+++ pages/About.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>Welcome to Spercompany</h2>
+      <p>Innovative Solutions for a Modern World.</p>
+    </div>
+  );
+}
+
+export default About;
