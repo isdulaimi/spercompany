a92a6034c656a715104f408f146a14f2c55dd4df
yaser-ali <yaser.ali32@hotmail.co.uk>
2025-06-21 01:35:19 +0100

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
-// src/pages/Home.js
 import React from 'react';
 
 function Home() {
-  return (
-    <div>
-      <h2>Welcome to Spercompany</h2>
-      <p>Innovative Solutions for a Modern World.</p>
-    </div>
-  );
+return (
+
+<div>
+
+<h2>Welcome to Spercompany</h2>
+
+<p>Innovative Solutions for a Modern World.</p>
+
+</div>
+
+);
+
 }
 
 export default Home;
