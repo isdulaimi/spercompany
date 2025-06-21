a4a2cbc498b4fa95a73ff2a8e390c88b9436477a
yaser-ali <yaser.ali32@hotmail.co.uk>
2025-06-22 00:56:45 +0100

    changes to products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -6,7 +6,6 @@ import img3 from './assets/3.jpg';
 import img4 from './assets/4.jpg';
 import img5 from './assets/5.jpg';
 import img6 from './assets/6.jpg';
-// استورد باقي الصور بنفس الطريقة
 
 
 const products = [
